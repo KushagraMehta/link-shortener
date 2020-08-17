bfba901b2b11895a4047c253e2d96007be85405d
Kushagra Mehta <kushagramehta2000@gmail.com>
2020-08-17 19:06:10 +0530

    testing gastby redirecting

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import { Link } from "gatsby";
 export default function page_404(props) {
-  // const URL = typeof window !== "undefined" ? window.location.href : "";
+  new Function(`window.location = "https://www.google.com";`)();
 
-  console.log(`${(window.location = "https://www.google.com")}`);
+  // console.log(`${(window.location = "https://www.google.com")}`);
   return <div>404 page not found</div>;
 }
